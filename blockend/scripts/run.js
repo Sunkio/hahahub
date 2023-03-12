19681c7d3bc54c428d944a4db07142a6a97b119a
Tanja Schmidt <taschmi@gmx.net>
2023-03-12 18:24:51 +0100

    add basic deploy script for local testchain

--- blockend/scripts/run.js
+++ blockend/scripts/run.js
@@ -1,17 +1,17 @@
 const main = async () => {
-  const jokeContractFactory = await hre.ethers.getContractFactory("JokeHub");
-  const jokeContract = await jokeContractFactory.deploy();
-  await jokeContract.deployed();
-  console.log("Contract deployed to:", jokeContract.address);
+  const jokeContractFactory = await hre.ethers.getContractFactory('JokeHub');
+    const jokeContract = await jokeContractFactory.deploy();
+    await jokeContract.deployed();
+    console.log("Contract deployed to:", jokeContract.address);
 };
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0); // exit Node process without error
+    process.exit(0);
   } catch (error) {
     console.log(error);
-    process.exit(1); // exit Node process with "Uncaught Fatal Exception" error
+    process.exit(1);
   }
 };
 
